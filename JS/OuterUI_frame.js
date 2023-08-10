3d056a14e3368df2fdcc253620e8d5c0ac3f665b
hirohiko <hirohiko.work@gmail.com>
2023-08-11 00:50:58 +0900

    ChangeTheContData'sPlace

--- JS/OuterUI_frame.js
+++ JS/OuterUI_frame.js
@@ -1,18 +1,29 @@
 // コンテンツデータにアクセス
 const contAceess = document.querySelectorAll("#contAceess_Box > article");
-const contData = document.querySelector("#contData_Block");
+const contOuter = document.querySelector("#contData_Outer");
 const contDataBG = document.querySelector("#contData_BlockBG");
 const contOffBtn = document.querySelector("#contOff_btn");
 const offTrigger = [contDataBG, contOffBtn];
 
-contAceess.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    contData.classList.add("show");
+// コンテンツデータ(本体)
+const contInner = document.querySelectorAll(".contData_inner");
+const ContInnerArray = Array.from(contInner);
+
+contAceess.forEach((element, i) => {
+  element.addEventListener("click", () => {
+    contOuter.classList.add("show");
+
+    contInner[i].classList.add("show");
   });
 });
 
 offTrigger.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    contData.classList.remove("show");
+  element.addEventListener("click", () => {
+    contOuter.classList.remove("show");
+
+    let target = ContInnerArray.find((data, index) => {
+      return data.classList.contains("show");
+    });
+    target.classList.remove("show");
   });
 });
